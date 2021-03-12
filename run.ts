3ce6d25027196078b74172e292f2774b7babf553
t0pth4t <dev@noliver.io>
2021-03-11 22:14:05 -0600

    stuff

--- run.ts
+++ run.ts
@@ -1,1 +1,56 @@
-console.info('yo')
+import * as config from "./config.json";
+import fetch from "node-fetch";
+import { Response } from "node-fetch";
+const getToken = async () => {
+  const requestOptions = {
+    method: "POST",
+  };
+
+  const response: Response = await fetch(
+    `http://${config.ipAddress}:16021/api/v1/new`,
+    { method: "POST" }
+  );
+  if (!response.ok) {
+    throw new Error(`${response.status} ${await response.json()}`);
+  }
+  return await response.json();
+};
+
+const getInfo = async (auth_token) => {
+  const response = await fetch(
+    `http://${config.ipAddress}:16021/api/v1/${auth_token}/`
+  );
+
+  if (!response.ok) {
+    throw new Error(`${response.status} ${await response.json()}`);
+  }
+  return await response.json();
+};
+
+const togglePower = async (auth_token: string, on: boolean) => {
+  const response = await fetch(
+    `http://${config.ipAddress}:16021/api/v1/${auth_token}/state`,
+    {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        on: {
+          value: on,
+        },
+      }),
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error(`${response.status} ${await response.json()}`);
+  }
+  return await response.json();
+};
+
+const run = async () => {
+  const { auth_token } = await getToken();
+  const info = await getInfo(auth_token);
+  console.log(JSON.stringify(info));
+  await togglePower(auth_token, false);
+  await togglePower(auth_token, true);
+};
