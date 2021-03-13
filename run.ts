66aa6b69c20e9d3c8f4cf4c63375792d9c8fa378
t0pth4t <dev@noliver.io>
2021-03-12 21:40:34 -0600

    wow it works

--- run.ts
+++ run.ts
@@ -7,7 +7,7 @@ const getToken = async () => {
   };
 
   const response: Response = await fetch(
-    `http://${config.ipAddress}:16021/api/v1/new`,
+    `http://${config.ipAddress}/api/v1/new`,
     { method: "POST" }
   );
   if (!response.ok) {
@@ -18,7 +18,7 @@ const getToken = async () => {
 
 const getInfo = async (auth_token) => {
   const response = await fetch(
-    `http://${config.ipAddress}:16021/api/v1/${auth_token}/`
+    `http://${config.ipAddress}/api/v1/${auth_token}/`
   );
 
   if (!response.ok) {
@@ -29,7 +29,7 @@ const getInfo = async (auth_token) => {
 
 const togglePower = async (auth_token: string, on: boolean) => {
   const response = await fetch(
-    `http://${config.ipAddress}:16021/api/v1/${auth_token}/state`,
+    `http://${config.ipAddress}/api/v1/${auth_token}/state`,
     {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
@@ -44,13 +44,35 @@ const togglePower = async (auth_token: string, on: boolean) => {
   if (!response.ok) {
     throw new Error(`${response.status} ${await response.json()}`);
   }
-  return await response.json();
+  return await response.text();
+};
+
+const setEffect = async (auth_token: string, effect: string) => {
+  const response = await fetch(
+    `http://${config.ipAddress}/api/v1/${auth_token}/effects`,
+    {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        select: effect,
+      }),
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error(`${response.status} ${await response.text()}`);
+  }
+  return await response.text();
 };
 
 const run = async () => {
-  const { auth_token } = await getToken();
-  const info = await getInfo(auth_token);
-  console.log(JSON.stringify(info));
-  await togglePower(auth_token, false);
-  await togglePower(auth_token, true);
+  //   const { auth_token } = await getToken();
+  const auth_token = config.auth_token;
+  //   const info = await getInfo(auth_token);
+  //   console.log(JSON.stringify(info));
+  //   await togglePower(auth_token, false);
+  //   await togglePower(auth_token, true);
+  await setEffect(auth_token, "Green");
 };
+
+run();
